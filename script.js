9b62173b9a4ef47e1fea6113bade1afcb2259af5
Felipe Lozano <david.loza010@gmail.com>
2023-07-11 10:29:38 -0400

    added functionality to modal buttons

--- script.js
+++ script.js
@@ -6,5 +6,20 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 
+for(let i = 0; i < btnsOpenModal.length; i++){
+    btnsOpenModal[i].addEventListener('click', function(){
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    })
+};
 
+btnCloseModal.addEventListener('click', function(){
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+});
+
+overlay.addEventListener('click', function(){
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+});
 
